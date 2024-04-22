328de78a2561687bfabaa13db993af7645fc2bf6
gauravsoni1812 <gauravsoni8800@gmail.com>
2024-04-23 01:42:37 +0530

    homepage and popup modal

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -3,10 +3,11 @@ import Link from 'next/link';
 // import { SignedIn, UserButton } from '@clerk/nextjs';
 
 import MobileNav from './MobileNav';
+import { SignedIn, UserButton } from '@clerk/nextjs';
 
 const Navbar = () => {
   return (
-    <nav className="flex-between fixed z-50 w-full bg-dark-1 px-6 py-4 lg:px-10">
+    <nav className="flex justify-between fixed z-50 w-full bg-dark-1 px-6 py-4 lg:px-10">
       <Link href="/" className="flex items-center gap-1">
         <Image
           src="/icons/logo.svg"
@@ -20,9 +21,9 @@ const Navbar = () => {
         </p>
       </Link>
       <div className="flex-between gap-5">
-        {/* <SignedIn>
+        <SignedIn>
           <UserButton afterSignOutUrl="/sign-in" />
-        </SignedIn> */}
+        </SignedIn>
 
         <MobileNav />
       </div>
